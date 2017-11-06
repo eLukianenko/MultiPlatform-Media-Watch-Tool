e62afe6994f4f0f5ff26f140f0d317a411872cfe
Evgeny Lukyanenko <xadjek@gmail.com>
2017-11-06 15:22:48 +0200

    created basic logic

---
+++ src/components/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <twitch :channel="streamName" :width="streamWidth" :height="streamHeight"></twitch>
+  </div>
+</template>
+<script>
+    import TwitchStream from './TwitchStream'
+
+export default {
+    name: 'HomePage',
+    components: {
+        'twitch': TwitchStream
+    },
+    data() {
+        return {
+            streamWidth: 854,
+            streamHeight: 480,
+            streamName: 'godhunt'
+        }
+    }
+}
+</script>
---
+++ src/components/TwitchStream.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="twitch-embed"></div>
+</template>
+<script>
+export default {
+    name: 'TwitchStream',
+    props: ['channel', 'width', 'height'],
+    mounted () {
+      new Twitch.Embed('twitch-embed', {
+          width: this.width,
+          height: this.height,
+          channel: this.channel,
+          layout: 'video',
+          autoplay: false
+      })
+    }
+}
+</script>
